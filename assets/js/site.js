a4703fbbc442c3b3a05591f892c03a3eeb6232d2
bryan schuetz <bschuetz@gmail.com>
2016-06-07 12:47:19 -0400

    More Better

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,6 +5,5 @@
 {% include _jsmods/scrollr.js %}
 {% include _jsmods/waypoints.js %}
 {% include _jsmods/modernizer.js %}
-{% include _jsmods/respond.js %}
 {% include _jsmods/isotope.js %}
 {% include _jsmods/flickity.js %}
