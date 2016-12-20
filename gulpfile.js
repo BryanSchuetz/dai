0d0f96ce6586738f8e9caaa4198e62b12b3fa576
bryan schuetz <bschuetz@gmail.com>
2016-12-20 11:27:24 -0500

    Fixes aggregation calls to secure digital site

--- gulpfile.js
+++ gulpfile.js
@@ -36,5 +36,5 @@ gulp.task('default', ['build', 'serve', 'watch']);
 
 // grab new content from the pubs and digital sites
 gulp.task('aggregate', shell.task([
-  'curl http://dai-global-developments.com/pubs.yml --output _data/pubs.yml && curl http://dai-global-digital.com/digital.yml --output _data/digital.yml'
+  'curl http://dai-global-developments.com/pubs.yml --output _data/pubs.yml && curl https://dai-global-digital.com/digital.yml --output _data/digital.yml'
 ]))
