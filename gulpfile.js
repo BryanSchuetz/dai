e7aec73a3e522bce4b2679b5bf2a7f7a180de364
bryan schuetz <bschuetz@gmail.com>
2017-03-21 12:33:29 -0400

    locking dependencies and moving to npm build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 // define all the things
 var gulp        = require('gulp');
-var bs          = require('browser-sync');
+// var bs          = require('browser-sync');
 // use this bit of middleware for connect server to clean up urls
-var hygienist   = require('hygienist-middleware');
-var reload      = bs.reload;
+// var hygienist   = require('hygienist-middleware');
+// var reload      = bs.reload;
 var shell       = require('gulp-shell');
 var uglify      = require('gulp-uglify');
 // exclude rendered files to avoid duplicate hits to the watch process
-var files = ['**/*.html', '**/*.md', '**/*.markdown', '**/*.js', '**/*.scss', '!build/**/*.*'];
-gulp.task('build', shell.task([
-  'JEKYLL_ENV=prod bundle exec jekyll build --incremental'
-]))
+// var files = ['**/*.html', '**/*.md', '**/*.markdown', '**/*.js', '**/*.scss', '!build/**/*.*'];
+// gulp.task('build', shell.task([
+//   'JEKYLL_ENV=prod bundle exec jekyll build --incremental'
+// ]))
 // setup browsersync to reload browser when changes happen
-gulp.task('serve', function() {
-  bs({
-      server:{
-        baseDir: "build",
-        middleware: hygienist("build")
-      },
-      notify: false,
-  });
-});
+// gulp.task('serve', function() {
+//   bs({
+//       server:{
+//         baseDir: "build",
+//         middleware: hygienist("build")
+//       },
+//       notify: false,
+//   });
+// });
 // compress javascript assets
 gulp.task('compress', function(){
   return gulp.src('build/assets/js/site.js')
@@ -28,11 +28,11 @@ gulp.task('compress', function(){
     .pipe(gulp.dest('build/assets/js'))
 });
 // look for changes to **source files**, then trigger a new incremental build and reload the browser
-gulp.task('watch', function(){
-  gulp.watch(files, ['build', reload]);
-})
+// gulp.task('watch', function(){
+//   gulp.watch(files, ['build', reload]);
+// })
 // the default task to call from the terminal: `gulp`
-gulp.task('default', ['build', 'serve', 'watch']);
+// gulp.task('default', ['build', 'serve', 'watch']);
 
 // grab new content from the pubs and digital sites
 gulp.task('aggregate', shell.task([
